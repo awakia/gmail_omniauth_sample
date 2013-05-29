bb4ac4380dc23cc8add7995a0ce8b1dddc44545c
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-29 15:53:52 +0900

    rails new gmail --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmail::Application.config.secret_key_base = 'a694f44a709e3ebeadc3de06b63dae34115f100db82b3bab2998865afe016b372799c81370c33650208412257c19d2cf70ad297c6f12b29cabc4144d65146221'

ed4b16cf15e7f25962b2f947af5fa7ca83c642b9
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-30 16:45:59 +0900

    Store email data to sqlite

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,3 +1,5 @@
+require 'nkf'
+
 class OmniauthCallbacksController < ApplicationController
   before_filter :basic_before_action
 
@@ -21,21 +23,42 @@ class OmniauthCallbacksController < ApplicationController
     count = gmail.inbox.count
 
     mails =  gmail.inbox.emails(:unread).map do |mail| #options: :all,:read,:unread
-      puts "Subject: #{mail.subject}"
-      puts "Date: #{mail.date}"
-      puts "From: #{mail.from}"
-      puts "To: #{mail.to}"
+      email = Email.new
+      email.from = mail_address(mail.from)
+      email.to = mail_address(mail.to)
+      email.subject = NKF::nkf('-wm', mail.subject)  # MIME decoding
+      email.date = mail.date
+      email.message_id = mail.message_id
+      email.in_reply_to = mail.in_reply_to
+      email.content_type = mail.content_type
+      email.charset = mail.charset
+      email.references = mail.references.to_s
+      email.header = mail.header.to_s
 
       #process body
       if mail.text_part
-        puts "text: " + mail.text_part.decoded
+        body = mail.text_part.decoded
       elsif mail.html_part
-        puts "html: " + mail.html_part.decoded
+        body = mail.html_part.decoded
       else
-        puts "body: " + mail.body.decoded.encode("UTF-8", mail.charset)
+        body = mail.body.decoded.encode("UTF-8", mail.charset)
       end
+
+      email.save!
     end
 
     binding.pry
+  end
+
+  def mail_address(adr)
+    if adr.is_a? Array
+      adr.map{ |x| mail_address(x) }.join(', ')
+    elsif adr.is_a? Net::IMAP::Address
+      res = ''
+      res += '"' + adr.name + '" ' if adr.name
+      res += "<#{adr.mailbox}@#{adr.host}>"
+    else
+      res.to_s
+    end
   end
 end

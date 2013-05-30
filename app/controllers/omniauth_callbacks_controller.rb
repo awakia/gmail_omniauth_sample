6e10156e2e985af4d02e4e5bfa0ee90684a8fc28
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2013-05-30 14:02:49 +0900

    Add to show mail content sample

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -19,6 +19,23 @@ class OmniauthCallbacksController < ApplicationController
     token = @omniauth.credentials.token
     gmail = Gmail.connect(:xoauth2, email, token)
     count = gmail.inbox.count
+
+    mails =  gmail.inbox.emails(:unread).map do |mail| #options: :all,:read,:unread
+      puts "Subject: #{mail.subject}"
+      puts "Date: #{mail.date}"
+      puts "From: #{mail.from}"
+      puts "To: #{mail.to}"
+
+      #process body
+      if mail.text_part
+        puts "text: " + mail.text_part.decoded
+      elsif mail.html_part
+        puts "html: " + mail.html_part.decoded
+      else
+        puts "body: " + mail.body.decoded.encode("UTF-8", mail.charset)
+      end
+    end
+
     binding.pry
   end
 end
